925f35b4cfff833c8332e3f4424f6aac74b54ed0
iamsu <sujay9626@gmail.com>
2024-10-21 19:56:21 -0700

    Adds TFT code (not working)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+
+#define SERIAL_BAUD_RATE 9600
+
+// peripheral objects
+TFT_eSPI tft = TFT_eSPI();
+
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+
+    Serial.begin(SERIAL_BAUD_RATE);
+    Serial.println("Initializing...");
+
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_WHITE);
+    tft.setCursor(0,0);
+    tft.setTextColor(TFT_WHITE);
+    tft.println("Hello from Irvine, CA");
+    Serial.println("All systems up");
 }
 
 void loop() {
-// write your code here
+    // write your code here
+
 }
