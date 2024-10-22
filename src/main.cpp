f96944840b3f2a119f3f9af2f22c41c3b761dfa8
iamsu <sujay9626@gmail.com>
2024-10-21 20:11:57 -0700

    Implements TFT Seconds elapsed counter

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,44 @@
 #include <TFT_eSPI.h>
 
 #define SERIAL_BAUD_RATE 9600
+#define SECOND 1
 
 // peripheral objects
 TFT_eSPI tft = TFT_eSPI();
 
+// other variables
+uint16_t clockSeconds = 0;
+
+void initTFT();
+
+void initBoardSerialBus();
+
 void setup() {
     // write your initialization code here
 
-    Serial.begin(SERIAL_BAUD_RATE);
+    initBoardSerialBus();
     Serial.println("Initializing...");
 
-    tft.init();
-    tft.setRotation(1);
-    tft.fillScreen(TFT_WHITE);
-    tft.setCursor(0,0);
-    tft.setTextColor(TFT_WHITE);
-    tft.println("Hello from Irvine, CA");
+    initTFT();
     Serial.println("All systems up");
 }
 
 void loop() {
     // write your code here
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.print(clockSeconds);
+    sleep(1 * SECOND);
+    clockSeconds++;
+}
 
+void initBoardSerialBus() { Serial.begin(SERIAL_BAUD_RATE); }
+
+void initTFT() {
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0,0);
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(3);
 }
